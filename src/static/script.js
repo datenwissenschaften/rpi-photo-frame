b40558397b8331930a44516099c81eec788961d2
Martin Franke <martin.franke@me.com>
2019-05-26 21:39:43 +0200

    +

--- src/static/script.js
+++ src/static/script.js
@@ -1,12 +1,16 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 
-    (function() {
+    (function () {
         $("#time").text(moment().format('DD.MM.YYYY HH:mm'));
         setTimeout(arguments.callee, 60000);
     })();
 
-    jQuery.getJSON("/weather", function(data) {
+    jQuery.getJSON("/weather", function (data) {
         $("#tempval").text(parseFloat(data.currently.temperature).toFixed(1))
+    });
+
+    jQuery("body").click(function () {
+        location.reload();
     });
 
 });
