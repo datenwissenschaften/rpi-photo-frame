c3cbdf3ec9ff5467d77a3f2b6dfa086998fe5e72
Martin Franke <martin.franke@me.com>
2019-06-13 16:56:13 +0200

    ++++++

--- src/scripts/bootstrap.sh
+++ src/scripts/bootstrap.sh
@@ -2,8 +2,8 @@
 
 /usr/bin/crontab /home/pi/rpi-photo-frame/src/cron/crontab
 
-/usr/bin/python3 /home/pi/rpi-photo-frame/src/app.py -d /home/pi/Downloads/ >> /var/log/rpi-photo-frame.log 2>&1
+/usr/bin/python3 /home/pi/rpi-photo-frame/src/app.py -d /home/pi/Downloads/ >> /var/log/rpi-photo-frame.log &
 
-/usr/local/bin/thumbor -c /home/pi/thumbor.conf >> /var/log/thumbor.log 2>&1
+/usr/local/bin/thumbor -c /home/pi/thumbor.conf >> /var/log/thumbor.log &
 
-/usr/local/bin/rpi-backlight --off 2>&1
+/usr/local/bin/rpi-backlight --off &
