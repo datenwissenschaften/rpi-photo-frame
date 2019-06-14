c488407853f8d799935a8cca08e26887ad6d6be9
Martin Franke <martin.franke@me.com>
2019-06-14 11:44:52 +0200

    Thumbor with face recognition.

--- src/scripts/bootstrap.sh
+++ src/scripts/bootstrap.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-/usr/bin/crontab /home/pi/rpi-photo-frame/src/cron/crontab
+/usr/bin/unclutter &
+
+/usr/bin/crontab /home/pi/rpi-photo-frame/src/cron/crontab &
 
 /usr/bin/python3 /home/pi/rpi-photo-frame/src/app.py -d /home/pi/Downloads/ >> /var/log/rpi-photo-frame.log &
 
-/usr/local/bin/thumbor -c /home/pi/thumbor.conf >> /var/log/thumbor.log &
+/usr/local/bin/thumbor -c /home/pi/rpi-photo-frame/src/conf/thumbor.conf >> /var/log/thumbor.log &
 
 /usr/local/bin/rpi-backlight --off &
