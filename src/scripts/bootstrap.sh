e27f8094bad0d835488465f5a2a5dbc224c8c4c1
Martin Franke <martin.franke@me.com>
2019-06-14 11:52:44 +0200

    feat(System): Adds unclutter to remove mouse cursor.

--- src/scripts/bootstrap.sh
+++ src/scripts/bootstrap.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-/usr/bin/unclutter &
+# Turn backlight off
+/usr/local/bin/rpi-backlight --off &
 
+# No mouse cursor
+export DISPLAY=:0 
+/usr/bin/unclutter -idle 0.1 -root &
+
+# Reload crontab from git
 /usr/bin/crontab /home/pi/rpi-photo-frame/src/cron/crontab &
 
-/usr/bin/python3 /home/pi/rpi-photo-frame/src/app.py -d /home/pi/Downloads/ >> /var/log/rpi-photo-frame.log &
-
+# Start thumbor
 /usr/local/bin/thumbor -c /home/pi/rpi-photo-frame/src/conf/thumbor.conf >> /var/log/thumbor.log &
 
-/usr/local/bin/rpi-backlight --off &
+# Start photo frame application
+/usr/bin/python3 /home/pi/rpi-photo-frame/src/app.py -d /home/pi/Downloads/ >> /var/log/rpi-photo-frame.log &
