db1facf2727403b4466de1f621e34124f173322c
Martin Franke <martin.franke@me.com>
2019-12-04 18:23:05 +0100

    Update xprograms.sh

--- src/scripts/xprograms.sh
+++ src/scripts/xprograms.sh
@@ -15,7 +15,7 @@ matchbox-window-manager &
 /usr/bin/fbi -T 1 -noverbose -a -t 30 --once /home/pi/rpi-photo-frame/doc/splash.png &
 
 # Start chromium
-/usr/bin/chromium-browser --noerrordialogs --incognito --disable-session-crashed-bubble --disable-infobars --force-device-scale-factor=1.00 --no-sandbox --kiosk "http://localhost:5000"
+/usr/bin/chromium-browser --noerrordialogs --incognito --disable-session-crashed-bubble --disable-infobars --force-device-scale-factor=1.00 --no-sandbox --kiosk "http://localhost:5600"
 
 # No mouse cursor
 DISPLAY=:0.0 ; export DISPLAY
