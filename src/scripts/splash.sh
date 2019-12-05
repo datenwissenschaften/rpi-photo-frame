38f835467bd2218cbe4f9b09a5c2571cd1b67a73
Martin Franke <martin.franke@me.com>
2019-12-05 19:07:02 +0100

    Cleanup - systemd.

--- src/scripts/splash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-DISPLAY=:0.0 ; export DISPLAY
-/usr/bin/fbi -T 1 -noverbose -a -t 30 --once /home/pi/rpi-photo-frame/doc/splash.png &
--- src/scripts/xprograms.sh
+++ src/scripts/xprograms.sh
@@ -14,10 +14,6 @@ matchbox-window-manager &
 # Start chromium
 /usr/bin/chromium-browser --noerrordialogs --incognito --disable-session-crashed-bubble --disable-infobars --force-device-scale-factor=1.00 --no-sandbox --kiosk "http://localhost:5600"
 
-# No mouse cursor
-DISPLAY=:0.0 ; export DISPLAY
-/usr/bin/unclutter -idle 0 -root &
-
 # Turn display on (force)
 /usr/bin/vcgencmd display_power 1
 
