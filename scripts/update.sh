5f863d5e194ae064f7d037c74c0c2a20f87f025f
Martin Franke <martin.franke@me.com>
2020-04-29 18:38:56 +0200

    Quick fix git.

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,6 +2,9 @@
 
 cd /home/pi/rpi-photo-frame/ || exit
 
+/usr/bin/git config --global user.email "you@example.com"
+/usr/bin/git config --global user.name "Your Name"
+
 /usr/bin/git reset --hard HEAD
 /usr/bin/git pull origin "${BRANCH}" | grep changed && /sbin/reboot
 
