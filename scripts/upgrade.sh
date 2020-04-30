980db249713cf90c336fa249da469a23831afd4a
Martin Franke <martin.franke@me.com>
2020-04-30 16:35:31 +0200

    no message

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -4,6 +4,8 @@
 
 apt install curl -y
 
+curl http://localhost:5600/toast/Update%20l%C3%A4uft.%20Bitte%20nicht%20ausschalten.%20%2010%%20
+
 # MIGRATION
 
 apt purge nodejs -y
@@ -11,6 +13,8 @@ chmod +x /usr/local/bin/uninstall-log2ram.sh && sudo /usr/local/bin/uninstall-lo
 apt purge plymouth -y
 apt purge profile-sync-daemon -y
 apt purge python* -y
+
+curl http://localhost:5600/toast/Update%20l%C3%A4uft.%20Bitte%20nicht%20ausschalten.%20%2030%%20
 
 # BOOTUP
 
@@ -23,12 +27,16 @@ systemctl disable hciuart.service
 systemctl disable raspi-config.service
 systemctl disable triggerhappy.service
 
+curl http://localhost:5600/toast/Update%20l%C3%A4uft.%20Bitte%20nicht%20ausschalten.%20%2040%%20
+
 # CONFIGURATIONS
 
 cp /home/pi/rpi-photo-frame/conf/rc.local /etc/rc.local
 chmod +x /etc/rc.local
 cp /home/pi/rpi-photo-frame/conf/config.txt /boot/config.txt
 /usr/bin/crontab /home/pi/rpi-photo-frame/cron/crontab
+
+curl http://localhost:5600/toast/Update%20l%C3%A4uft.%20Bitte%20nicht%20ausschalten.%20%2050%%20
 
 # HOUSEKEEPING
 
@@ -48,6 +56,8 @@ apt autoremove --purge "$(deborphan)" -y
 apt autoremove --purge -y
 apt autoclean -y
 
+curl http://localhost:5600/toast/Update%20l%C3%A4uft.%20Bitte%20nicht%20ausschalten.%20%2070%%20
+
 # MINIMAL UI
 
 apt install fbi -y
@@ -55,6 +65,8 @@ apt install midori matchbox -y
 apt install chromium-browser -y
 apt install unclutter -y
 apt install git -y
+
+curl http://localhost:5600/toast/Update%20l%C3%A4uft.%20Bitte%20nicht%20ausschalten.%20%2075%%20
 
 # PYTHON DEV PACKAGES
 
@@ -68,6 +80,8 @@ apt install libffi-devel -y
 apt install python-cffi python-cryptography -y
 apt install python3-cffi python3-cryptography python3-numpy python3-pillow python3-dev -y
 
+curl http://localhost:5600/toast/Update%20l%C3%A4uft.%20Bitte%20nicht%20ausschalten.%20%2080%%20
+
 # http://localhost:5600/toast/...%20Update%20l%C3%A4uft.%20Bitte%20nicht%20ausschalten%20..%2060%%20
 
 # PHOTO FRAME
@@ -76,5 +90,7 @@ python3.5 -m pip install --upgrade pip
 cd /home/pi/rpi-photo-frame || exit
 python3.5 -m pip install -r requirements.txt --upgrade
 python3.5 -m pip install gunicorn --upgrade
+
+curl http://localhost:5600/toast/Update%20l%C3%A4uft.%20Bitte%20nicht%20ausschalten.%20%20100%%20
 
 reboot 0
