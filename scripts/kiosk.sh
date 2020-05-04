3c5b1bd5113b4d5494501cdc2e430bbc49a3eeac
Martin Franke <martin.franke@me.com>
2020-05-04 17:22:32 +0200

    no message

--- scripts/kiosk.sh
+++ scripts/kiosk.sh
@@ -3,18 +3,10 @@
 unclutter -idle 0.5 -root &
 
 xset -dpms
-xset s activate
-xset s 1
-
-/usr/bin/chromium-browser --noerrdialogs --incognito --disable-session-crashed-bubble --disable-infobars --force-device-scale-factor=1.00 --kiosk "http://localhost:5600" &
-
-sleep 30
-xset s reset
-xset s 0
-
-xset -dpms
 xset s off
 xset s noblank
+
+/usr/bin/chromium-browser --noerrdialogs --incognito --disable-session-crashed-bubble --disable-infobars --force-device-scale-factor=1.00 --kiosk "http://localhost:5600" &
 
 while true; do
   sleep 36000
