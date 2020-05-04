328a997f7e74eb3952ea9a95cef862ba107876ee
Martin Franke <martin.franke@me.com>
2020-05-04 16:12:25 +0200

    no message

--- scripts/install.sh
+++ scripts/install.sh
@@ -64,10 +64,6 @@ chmod -R 777 /home/pi/rpi-photo-frame
 
 cp /home/pi/rpi-photo-frame/scripts/kiosk.service /lib/systemd/system/kiosk.service
 systemctl daemon-reload
-systemctl stop kiosk.service
-systemctl disable kiosk.service
-systemctl daemon-reload
-systemctl start kiosk.service
 systemctl enable kiosk.service
 
 # REBOOT
--- scripts/kiosk.sh
+++ scripts/kiosk.sh
@@ -5,8 +5,8 @@ xset -dpms
 xset s off
 xset s noblank
 
-# /usr/bin/fbi -T 1 -noverbose -a -t 5 --once /home/pi/rpi-photo-frame/doc/splash.png &
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk "http://localhost:5600" &
+/usr/bin/fbi -T 1 -noverbose -a -t 10 --once /home/pi/rpi-photo-frame/doc/splash.png &
+/usr/bin/chromium-browser --noerrdialogs --incognito --disable-session-crashed-bubble --disable-infobars --force-device-scale-factor=1.00 --kiosk "http://localhost:5600" &
 
 while true; do
   sleep 36000
