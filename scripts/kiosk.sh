347a5b642c202ba02a2b0248d7d162f05982c528
Martin Franke <martin.franke@me.com>
2020-05-04 16:21:58 +0200

    no message

--- scripts/kiosk.sh
+++ scripts/kiosk.sh
@@ -5,7 +5,6 @@ xset -dpms
 xset s off
 xset s noblank
 
-/usr/bin/fbi -T 1 -noverbose -a -t 10 --once /home/pi/rpi-photo-frame/doc/splash.png &
 /usr/bin/chromium-browser --noerrdialogs --incognito --disable-session-crashed-bubble --disable-infobars --force-device-scale-factor=1.00 --kiosk "http://localhost:5600" &
 
 while true; do
