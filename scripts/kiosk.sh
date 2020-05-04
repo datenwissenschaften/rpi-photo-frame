cd80919ce464e29ed4d8309b9f59d22c834bff0f
Martin Franke <martin.franke@me.com>
2020-05-04 15:58:58 +0200

    no message

--- scripts/kiosk.sh
+++ scripts/kiosk.sh
@@ -6,9 +6,8 @@ xset s off
 xset s noblank
 
 # /usr/bin/fbi -T 1 -noverbose -a -t 5 --once /home/pi/rpi-photo-frame/doc/splash.png &
-/usr/bin/chromium-browser --noerrordialogs --incognito --disable-session-crashed-bubble --disable-infobars --force-device-scale-factor=1.00 --no-sandbox --kiosk --test-type "http://localhost:5600" &
+/usr/bin/chromium-browser --noerrordialogs --incognito --disable-session-crashed-bubble --disable-infobars --force-device-scale-factor=1.00 --kiosk http://localhost:5600 &
 
 while true; do
-   xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
-   sleep 3600
+  sleep 36000
 done
