49d1ac649efb803858f11616f37d88a65ea777e0
Martin Franke <martin.franke@me.com>
2020-05-04 17:49:53 +0200

    no message

--- scripts/kiosk.sh
+++ scripts/kiosk.sh
@@ -6,6 +6,7 @@ xset -dpms
 xset s off
 xset s noblank
 
+/usr/bin/python3 /home/pi/rpi-photo-frame/server/manage.py >> /var/log/photo-frame.log &
 /usr/bin/chromium-browser --noerrdialogs --incognito --disable-session-crashed-bubble --disable-infobars --force-device-scale-factor=1.00 --kiosk "http://localhost:5600" &
 
 while true; do
