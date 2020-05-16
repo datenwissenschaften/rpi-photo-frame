c846dd0780ed531966078aba173457ee559c4f64
Martin Franke <martin.franke@me.com>
2020-05-16 22:26:54 +0200

    New git check.

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,4 +6,11 @@ cd /home/pi/rpi-photo-frame/ || exit
 /usr/bin/git config --global user.name "Martin Franke"
 
 /usr/bin/git reset --hard HEAD
-/usr/bin/git pull origin "${BRANCH}" | grep changed && sh /home/pi/rpi-photo-frame/scripts/install.sh
+
+CHANGED=$(/usr/bin/git status --porcelain)
+
+if [ -n "${CHANGED}" ]; then
+  sh /home/pi/rpi-photo-frame/scripts/install.sh
+else
+  echo ""
+fi
