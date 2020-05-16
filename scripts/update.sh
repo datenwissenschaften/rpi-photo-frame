fd5bcd986cff52c31420677a61de3f03a9928130
Martin Franke <martin.franke@me.com>
2020-05-16 22:54:46 +0200

    Git update mechanism.

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,12 +7,13 @@ cd /home/pi/rpi-photo-frame/ || exit
 
 /usr/bin/git clean -f -d
 /usr/bin/git reset --hard HEAD
+/usr/bin/git fetch
 
-CHANGED=$(/usr/bin/git status --porcelain --untracked-files=no)
-
-if [ -n "${CHANGED}" ]; then
+CHANGED=0
+/usr/bin/git remote update && /usr/bin/git status -uno | grep -q 'Your branch is behind' && CHANGED=1
+if [ $CHANGED = 1 ]; then
   /usr/bin/git pull origin "${BRANCH}"
   /bin/bash /home/pi/rpi-photo-frame/scripts/install.sh
 else
-  echo ""
+    echo ""
 fi
