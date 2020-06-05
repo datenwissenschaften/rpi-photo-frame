b4adfc618ee842af592541bc13df048b3d3833a7
Martin Franke <martin.franke@me.com>
2020-06-05 11:46:11 +0200

    Yet another cropper.

--- scripts/kiosk.sh
+++ scripts/kiosk.sh
@@ -9,7 +9,7 @@ xset -dpms
 xset s off
 xset s noblank
 
-sleep 30
+sleep 5
 
 /usr/bin/chromium-browser --noerrdialogs --incognito --disable-session-crashed-bubble --disable-infobars --force-device-scale-factor=1.00 --kiosk "http://localhost:5600" &
 
--- server/image/cropper.py
+++ server/image/cropper.py
@@ -1,12 +1,5 @@
+import os
 import tempfile
-
-import cv2
-
-try:
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
-from Katna.image import Image as IM
 
 
 class Cropper:
@@ -15,23 +8,7 @@ class Cropper:
         self.crop_y = 800
 
     def crop(self, image_path):
-        img_module = IM()
-
-        crop_list = img_module.crop_image(
-            file_path=image_path,
-            crop_width=self.crop_x,
-            crop_height=self.crop_y,
-            num_of_crops=1,
-            down_sample_factor=8
-        )
-
-        for counter, crop in enumerate(crop_list):
-            img_module.save_crop_to_disk(
-                crop,
-                cv2.imread(image_path),
-                file_path=tempfile.gettempdir(),
-                file_name="_working_image_",
-                file_ext=".jpeg",
-            )
+        os.system("smartcroppy --width " + str(self.crop_x) + " --height "
+                  + str(self.crop_y) + " " + image_path + " " + tempfile.gettempdir() + "/_working_image_.jpeg")
 
         return tempfile.gettempdir() + "/_working_image_.jpeg"
