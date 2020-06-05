3a814a503daf89b39ce3af0e6a142739f996e695
Martin Franke <martin.franke@me.com>
2020-06-05 11:02:56 +0200

    Test updater.

--- scripts/kiosk.sh
+++ scripts/kiosk.sh
@@ -9,7 +9,7 @@ xset -dpms
 xset s off
 xset s noblank
 
-sleep 5
+sleep 30
 
 /usr/bin/chromium-browser --noerrdialogs --incognito --disable-session-crashed-bubble --disable-infobars --force-device-scale-factor=1.00 --kiosk "http://localhost:5600" &
 
