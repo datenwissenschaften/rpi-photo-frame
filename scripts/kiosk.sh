068a40f08129a5c60f91ec91759acc7fed181834
Martin Franke <martin.franke@me.com>
2022-04-15 20:19:40 +0200

    Update kiosk.sh

--- scripts/kiosk.sh
+++ scripts/kiosk.sh
@@ -19,7 +19,7 @@ sudo thumbor -c /home/pi/rpi-photo-frame/scripts/thumbor.conf >/home/pi/thumbor.
 ### RPI-PI-PHOTOFRAME
 
 MASTER_IP=$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')
-sudo /opt/rpi-photo-frame-1.5.0/bin/rpi-photo-frame -Dmaster.ip="$MASTER_IP" -Dconfig.file=/opt/rpi-photo-frame-1.5.0/conf  &
+sudo /opt/rpi-photo-frame-1.5.0/bin/rpi-photo-frame -Dmaster.ip="$MASTER_IP" -Dconfig.file=/opt/rpi-photo-frame-1.5.0/conf/secret.conf &
 
 ### KIOSK CHROME
 
