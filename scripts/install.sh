c0df78a149c7f57cc1c76b76f2b3167f7665b77c
Dr. Martin Franke <martin.franke@me.com>
2022-10-15 16:21:09 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -27,15 +27,8 @@ apt autoremove -y
 
 curl http://localhost:5600/toast/Update%20l%C3%A4uft.%20Bitte%20nicht%20ausschalten.%20%2020%%20
 
-apt install curl xdotool unclutter sed git fbi python3-pip libatlas-base-dev libjpeg-dev zlib1g-dev libfreetype6-dev liblcms1-dev libopenjp2-7 libtiff5 python-cffi python-cryptography python3-cffi python3-cryptography python3-numpy python3-pillow python3-dev libhdf5-dev libhdf5-serial-dev libjasper-dev libjasper-dev python3-h5py -y
-
-apt install libcblas-dev -y
-apt install libhdf5-dev -y
-apt install libhdf5-serial-dev -y
-apt install libatlas-base-dev -y
-apt install libjasper-dev -y
-apt install libqtgui4 -y
-apt install libqt4-test -y
+apt install curl xdotool unclutter sed git fbi python3-pip \
+  python3-cffi python3-cryptography python3-numpy python3-pillow -y
 
 curl http://localhost:5600/toast/Update%20l%C3%A4uft.%20Bitte%20nicht%20ausschalten.%20%2035%%20
 
